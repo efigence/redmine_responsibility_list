8a7bbd77bd427a0eb7152075881d576b0ee5874c
Jacek Grzybowski <jgrzybowski@artegence.com>
2013-11-25 18:18:27 +0100

    better looking setting page

--- assets/javascripts/my_selectize.js
+++ assets/javascripts/my_selectize.js
@@ -1,16 +1,23 @@
 $(function() {
   $('#groups').selectize({
+    plugins: ['remove_button']
   });
   $('#names1').selectize({
+    plugins: ['remove_button']
   });
   $('#names2').selectize({
+    plugins: ['remove_button']
   });
   $('#names3').selectize({
+    plugins: ['remove_button']
   });
   $('#names4').selectize({
+    plugins: ['remove_button']
   });
   $('#names5').selectize({
+    plugins: ['remove_button']
   });
   $('#custom_field').selectize({
+    plugins: ['remove_button']
   });
 });
