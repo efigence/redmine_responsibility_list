829a4caf614274ff79a902fa69e1e0f32fc28828
jatska <jacek213@gmail.com>
2013-12-10 18:16:46 +0100

    user query

--- app/helpers/list_helper.rb
+++ app/helpers/list_helper.rb
@@ -3,10 +3,10 @@ module ListHelper
   def multiple_user_link(user_logins)
     return nil if user_logins.empty?
     str = ''
-    user_logins.each_with_index do |login, i|
-      user = User.find_by_login(login)
-      str << link_to("#{user.name} (#{user.login})", user_path(user))
-      str << ', ' unless i == user_logins.size - 1
+    users = User.select([:id, :login, :firstname, :lastname]).where(login: user_logins)
+    users.each do |user|
+      str << link_to("#{user.to_s} (#{user.login})", user_path(user))
+      str << ', ' unless users.last == user
     end
     str.html_safe
   end
